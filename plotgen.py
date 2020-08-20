8c69e2e14aeccb8480fdd5ec96e46a4edbc655e4
M.P.Ross <mpross2@uw.edu>
2020-08-20 16:30:19 -0700

    Changed some visuals.

--- plotgen.py
+++ plotgen.py
@@ -1,7 +1,9 @@
 import numpy as np
 import os
 
-import matplotlib.pyplot as plt, mpld3
+import matplotlib.pyplot as plt
+import mpld3
+from mpld3 import plugins
 
 fig = plt.figure(1)
 
@@ -20,14 +22,27 @@ for filename in os.listdir('Data'):
     
     f.close()
     
-    plt.loglog(freq,lim,label=filename)
+    if filename[:-4]=='EarthMode':
+        
+        plt.plot(freq,lim, color='k',label=filename)
+        plt.text(freq[-1],lim[0]*10,filename[:-4])
+    else:   
+        
+        plt.loglog(freq,lim,color='k',label=filename)
+
+        if len(lim)<10:
+            plt.text(freq[0],lim[0]*10,filename[:-4])
+        else:
+            if lim[0]<lim[-1]:
+                plt.text(freq[0],lim[-1],filename[:-4])
+            else:
+                plt.text(freq[-1]/100,lim[-1]*100,filename[:-4])
 
 plt.grid()
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("$\Omega_{GW}$")
-plt.legend()
 
-fig.set_size_inches(7, 5)
+
 
 html = mpld3.fig_to_html(fig)
 
